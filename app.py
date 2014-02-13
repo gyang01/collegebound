40b58d48fd18e7d5f3479629528763b6ea108a8f
gyang01 <georgiana.yang@yahoo.com>
2014-02-12 23:35:54 -0500

    main.html and static files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_templates,url_for,requests,redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(port=8888)
